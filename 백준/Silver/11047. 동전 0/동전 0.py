59a630973253b92aade92594acfd0c48042382d6
hyobin0726 <140376727+hyobin0726@users.noreply.github.com>
2023-10-30 11:37:46 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -4,9 +4,8 @@ sum=0
 for i in range(n):
   n_list.append(int(input()))
 
-while k!=0:
-  for i in reversed(range(n)):
-    if k//n_list[i] >=1:
-      sum+=k//n_list[i]
-      k=k%n_list[i]
+
+for i in reversed(range(n)):
+  sum+=k//n_list[i]
+  k=k%n_list[i]
 print(sum)
