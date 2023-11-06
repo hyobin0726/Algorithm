9e1a6a3c28b351ae0ebfc1992eb335755e3e0803
hyobin0726 <140376727+hyobin0726@users.noreply.github.com>
2023-11-06 21:21:18 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 162 ms, Memory: 61,728 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
@@ -1,13 +1,13 @@
 for test_case in range(1,11):
-    result = 0
-    houseCount = int(input())
-    house = list(map(int , input().split()))
-    for i in range(2, houseCount-2):
-        def_2 = house[i] - house[i-2]
-        def_1 = house[i] - house[i-1]
-        def1 = house[i] - house[i+1]
-        def2 = house[i] - house[i+2]
-        if def_2 > 0 and def_1 > 0 and def1 > 0 and def2 > 0 :
-            result += min(def_2, def_1, def1, def2)
+  n=int(input())
+  graph=list(map(int,input().split()))
+  cnt=0
+  for i in range(2,n-2):
+    d1=graph[i]-graph[i-2]
+    d2=graph[i]-graph[i-1]
+    d3=graph[i]-graph[i+1]
+    d4=graph[i]-graph[i+2]
+    if d1>0and d2>0 and d3>0 and d4>0:
+      cnt+=min(d1,d2,d3,d4)
 
-    print("#{} {}".format(test_case,result))
+  print("#{} {}".format(test_case,cnt))
