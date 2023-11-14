0a82d4c9cf475d28d655a10b2140ca95f828f240
hyobin0726 <140376727+hyobin0726@users.noreply.github.com>
2023-11-14 15:34:35 +0900

    [D2] Title: 간단한 소인수분해, Time: 149 ms, Memory: 57,460 KB -BaekjoonHub

---
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
@@ -0,0 +1,26 @@
+T=int(input())
+for tc in range(1,T+1):
+    
+    n=int(input())
+    a,b,c,d,e=0,0,0,0,0
+    
+    while n%2 ==0:
+        a+=1
+        n=(n//2)
+    
+    while n%3 ==0:
+        b+=1
+        n=(n//3)
+    
+    while n%5 ==0:
+        c+=1
+        n=(n//5)
+    
+    while n%7 ==0:
+        d+=1
+        n = (n // 7)
+    
+    while n%11 ==0:
+        e+=1
+        n = (n // 11)
+    print("#{} {} {} {} {} {}".format(tc, a,b,c,d,e))
