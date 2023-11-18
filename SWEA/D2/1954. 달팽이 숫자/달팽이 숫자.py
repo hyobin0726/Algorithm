4b5364ad435360c71444bca416a4d8335c225c1e
hyobin0726 <140376727+hyobin0726@users.noreply.github.com>
2023-11-18 10:43:40 +0900

    [D2] Title: 달팽이 숫자, Time: 148 ms, Memory: 58,532 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -1,30 +1,26 @@
-T = int(input())
-for tc in range(1,T+1):
-  n=int(input())
-  board = [[0 for _ in range(n)] for  _ in range(n)]
-  
-  #좌,하,우,상
-  dx=[0,1,0,-1]
-  dy=[1,0,-1,0]
-  
-  x,y=0,0
-  dist=0 #좌:0
-  
-  for i in range(1, n*n+1):
-    board[x][y] = i
-    x=x+dx[dist]
-    y=y+dy[dist]
-    
-    
-    if x<0 or x>=n or y<0 or y>=n or board[x][y]!=0:
-      x -= dx[dist]
-      y -= dy[dist]
-    
-      dist = (dist+1)%4
-      
-      x += dx[dist]
-      y += dy[dist]
-  print("#{}".format(tc))  
-  for i in board:
-    print(*i)
-       
+T=int(input())
+for tc in range (1,T+1):
+    n=int(input())
+    graph=[[0]*n for _ in range (n)]
+    cnt=0
+    result=[[]*n for _ in range(n)]
+    for i in range (n):
+        for j in range (n):
+            if graph[i][j]==0:
+                cnt+=1
+                graph[i][j]=cnt
+        for k in range (n):
+            if graph[k][j] == 0:
+                cnt+=1
+                graph[k][j]=cnt
+        for l in range (n-1,-1,-1):
+            if graph[k][l] == 0:
+                cnt+=1
+                graph[k][l]=cnt
+        for m in range ( n-1,-1,-1):
+            if graph[m][l] == 0:
+                cnt+=1
+                graph[m][l]=cnt
+    print("#{}".format(tc))
+    for i in range(n):
+        print(*graph[i])
