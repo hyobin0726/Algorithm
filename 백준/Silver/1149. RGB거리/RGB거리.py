90b538e4a37411c2b544bc1ac3f200981309702c
hyobin0726 <140376727+hyobin0726@users.noreply.github.com>
2024-01-05 14:49:50 +0900

    [Silver I] Title: RGB거리, Time: 76 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -0,0 +1,11 @@
+n=int(input())
+cost=[0]*n
+for i in range (n):
+    cost[i]=list(map(int,input().split()))
+
+for i in range (1,n):
+    cost[i][0]=min(cost[i-1][1],cost[i-1][2])+cost[i][0]
+    cost[i][1]=min(cost[i-1][0],cost[i-1][2])+cost[i][1]
+    cost[i][2]=min(cost[i-1][1],cost[i-1][0])+cost[i][2]
+
+print(min(cost[n-1]))
