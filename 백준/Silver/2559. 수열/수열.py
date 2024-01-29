41981ae5484250f2d1e806ae967c39c17887760e
hyobin0726 <140376727+hyobin0726@users.noreply.github.com>
2024-01-29 16:44:33 +0900

    [Silver III] Title: 수열, Time: 76 ms, Memory: 40156 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,12 +1,11 @@
-n,k=map(int,input().split())
-num=list(map(int,input().split()))
-temp_sum=sum(num[:k])
-total=temp_sum
+N, K = map(int, input().split())
+tem_list = list(map(int, input().split()))
 
+part_sum = sum(tem_list[:K])
+result_list = [part_sum]
 
-for i in range(n-k):
-    temp_sum = temp_sum - num[i] + num[i+k]
+for i in range(0, len(tem_list)-K):
+    part_sum = part_sum - tem_list[i] + tem_list[i+K]
+    result_list.append(part_sum)
 
-    if temp_sum>total:
-        total=temp_sum
-print(total)
+print(max(result_list))
