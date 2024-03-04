1a0de4ff10cdf2415dcf7463278c63bc054553de
hyobin0726 <140376727+hyobin0726@users.noreply.github.com>
2024-03-04 23:49:21 +0900

    [Silver II] Title: 베르트랑 공준, Time: 3268 ms, Memory: 111120 KB -BaekjoonHub

---
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -0,0 +1,16 @@
+while True:
+    n=int(input())
+    cnt=0
+    if n ==0:
+        break
+    for i in range(n+1,2*n+1):
+        if i == 1 :
+            continue
+
+        for j in range(2,int(i**0.5)+1):
+            if i%j == 0 :
+                break
+        else:
+            cnt+=1
+
+    print(cnt)
