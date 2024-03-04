b6de0840c3ea63ae6a38a5ce1233a31150bd9fde
hyobin0726 <140376727+hyobin0726@users.noreply.github.com>
2024-03-05 00:15:41 +0900

    [Silver II] Title: 베르트랑 공준, Time: 1536 ms, Memory: 33048 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -1,16 +1,21 @@
+num=123456*2+1
+prime_list=[0]*num
+
+for i in range(1,num):
+    if i == 1 :
+        continue
+
+    for j in range(2,int(i**0.5)+1):
+        if i%j == 0 :
+            break
+    else:
+        prime_list[i]=1
+
 while True:
     n=int(input())
-    cnt=0
     if n ==0:
         break
+    cnt=0
     for i in range(n+1,2*n+1):
-        if i == 1 :
-            continue
-
-        for j in range(2,int(i**0.5)+1):
-            if i%j == 0 :
-                break
-        else:
-            cnt+=1
-
+        cnt+=prime_list[i]
     print(cnt)
