6f181cc77f4baec2d0b4900a14a1124200baaddc
hyobin <140376727+hyobin0726@users.noreply.github.com>
2024-05-26 15:57:32 +0900

    [level 2] Title: 가장 큰 수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -0,0 +1,13 @@
+
+def solution(numbers):
+    answer = ''
+
+    for i in range (len(numbers)):
+        numbers[i]=str(numbers[i])
+    numbers.sort(key = lambda x : x*4 , reverse=True)
+    answer=''.join(numbers)
+    if answer[0] == '0':
+        answer ='0'
+    
+    
+    return answer
