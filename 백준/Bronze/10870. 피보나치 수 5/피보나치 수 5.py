6d3e02b29baedcc6708ad05bcfb6d92c644408c9
hyobin <140376727+hyobin0726@users.noreply.github.com>
2024-05-29 19:34:45 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -0,0 +1,12 @@
+n=int(input())
+f=[0]*21
+
+def fibo(x):
+    if x==0 :
+        return 0
+    if x==1:
+        return 1
+    if f[x] == 0:
+        f[x]=fibo(x-1)+fibo(x-2)
+    return f[x]
+print(fibo(n))
