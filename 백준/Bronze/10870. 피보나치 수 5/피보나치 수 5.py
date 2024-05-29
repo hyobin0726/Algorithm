9f6f650b5437567c9e00925c7414818b454f5f6b
hyobin <140376727+hyobin0726@users.noreply.github.com>
2024-05-29 19:34:58 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -1,12 +1,11 @@
 n=int(input())
 f=[0]*21
+f[0]=0
+f[1]=1
+def fibo(n):
+    for i in range(2,n+1):
+        if f[i] ==0 :
+            f[i]=f[i-1]+f[i-2]
 
-def fibo(x):
-    if x==0 :
-        return 0
-    if x==1:
-        return 1
-    if f[x] == 0:
-        f[x]=fibo(x-1)+fibo(x-2)
-    return f[x]
+    return f[n]
 print(fibo(n))
