7c0a7bb89f9be01133f2ba895541180f964c85cd
hyobin <140376727+hyobin0726@users.noreply.github.com>
2024-08-01 23:16:22 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 72 ms, Memory: 11532 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -1,49 +1,53 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int n;
-    static List<int[]> arr = new ArrayList<>();
-    static List<int[]> temp = new ArrayList<>();
-    static List<Integer> result = new ArrayList<>();
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n= Integer.parseInt(br.readLine());
 
-        for(int i =0 ; i<n; i++){
-            StringTokenizer st=new StringTokenizer(br.readLine());
-            int x =Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            arr.add(new int[] {x,y});
-        }
-        for (int i=1; i< n+1;i++){
-            dfs(0,i);
-        }
-        System.out.println(Collections.min(result));
-    }
-    static void dfs(int start,int count){
-        if (count == temp.size()){
-            taste(temp);
-            return;
-        }
-        for (int i = start; i<arr.size();i++){
-            if (!temp.contains(arr.get(i))){
-                temp.add(arr.get(i));
-                dfs(i+1,count);
-                temp.remove(arr.get(i));
-            }
-        }
+	
+	static int minTaste;
+	static boolean[] isSelected;
+	static int[][] tastes;
+	static int N;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		isSelected = new boolean[N];
+		minTaste = Integer.MAX_VALUE;
+		tastes = new int[N][2];
+		
+		for(int i=0;i<N;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			tastes[i][0] = Integer.parseInt(st.nextToken());
+			tastes[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		
+		subSet(0,1,0);
+		
+		System.out.println(minTaste);
+	}
+	
+	public static void subSet(int cnt, int mulSour, int sumBitter) {
+		if(cnt == N) {
+			int falseCnt = 0;
+			for(int i=0;i<N;i++) {
+				if(isSelected[i]) continue;
+				falseCnt++;
+			}
+			if(falseCnt == N) return;
+			minTaste = Math.min(minTaste, Math.abs(mulSour - sumBitter));
+			return;
+		}
+		
+		isSelected[cnt] = true;
+		subSet(cnt+1,mulSour*tastes[cnt][0],sumBitter+tastes[cnt][1] );
+		isSelected[cnt] = false;
+		subSet(cnt+1,mulSour,sumBitter);
+		
+		
+	}
 
-    }
-    static void taste(List<int[]> taste){
-        int x =1;
-        int y =0;
-
-        for (int[] t : taste){
-            x*=t[0];
-            y+=t[1];
-        }
-        result.add(Math.abs(x-y));
-
-    }
 }
