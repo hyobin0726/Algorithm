3e35b792aad938fc8fa352ebd621d9e4cb976aba
hyobin <140376727+hyobin0726@users.noreply.github.com>
2024-08-17 16:11:41 +0900

    [D3] Title: 햄버거 다이어트, Time: 957 ms, Memory: 87,508 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
@@ -1,23 +1,37 @@
-T=int(input())
-def dfs(i, sum,score):
-    global cnt,total
-    if i ==n:
-        if sum <= l:
-            cnt.append(sum)
-            total.append(score)
-        return
-    dfs(i + 1, sum, score)
-    dfs(i + 1, sum + b_list[i],score+a_list[i])
 
-for tc in range(1,T+1):
-    n,l=map(int,input().split())
-    a_list=[]
-    b_list=[]
-    for i in range (n) :
-        a,b=map(int, input().split())
-        a_list.append(a)
-        b_list.append(b)
-    cnt=[]
-    total=[]
-    dfs(0,0,0)
-    print("#{} {}".format(tc,max(total)))
+import java.io.*;
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        for (int test = 1; test < t+1; test++) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int l = Integer.parseInt(st.nextToken());
+            int[] score = new int[n + 1];
+            int[] kcal = new int[n + 1];
+
+            for (int i = 1; i <= n; i++) {
+                st = new StringTokenizer(br.readLine());
+                score[i] = Integer.parseInt(st.nextToken());
+                kcal[i] = Integer.parseInt(st.nextToken());
+
+            }
+            int[][] dp = new int[n + 1][l + 1];
+            for (int i = 1; i < n + 1; i++) {
+                for (int j = 0; j <= l; j++) {
+                    if (j < kcal[i]) {
+                        dp[i][j] = dp[i - 1][j];
+                    } else {
+                        dp[i][j] = Math.max(dp[i - 1][j - kcal[i]] + score[i], dp[i - 1][j]);
+
+                    }
+                }
+            }
+            System.out.println("#" + test + " "+  dp[n][l]);
+        }
+    }
+}
