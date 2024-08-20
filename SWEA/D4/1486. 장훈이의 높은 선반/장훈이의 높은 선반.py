17b1667d18145219aaae5eeb80bb5a5901009ae1
hyobin <140376727+hyobin0726@users.noreply.github.com>
2024-08-20 11:54:41 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 272 ms, Memory: 66,228 KB -BaekjoonHub

--- SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.py
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.py
@@ -1,17 +1,48 @@
-T=int(input())
-def dfs(i, sum):
-    global cnt
-    if i ==n:
-        if sum >= b:
-            cnt.append(sum)
-        return
-    dfs(i + 1, sum)
-    dfs(i + 1, sum + a[i])
-
-for tc in range(1,T+1):
-    n,b=map(int,input().split())
-    a=[]
-    a = list(map(int, input().split()))
-    cnt=[]
-    dfs(0,0)
-    print("#{} {}".format(tc,min(cnt)-b))
+import java.io.*;
+import java.util.*;
+public class Solution {
+	static int n;
+	static int b;
+	static int[] tall;
+	static int result;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		for (int test =1; test<t+1; test++) {
+			result = Integer.MAX_VALUE;
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+			tall = new int[n];
+			st = new StringTokenizer(br.readLine());
+			for (int i =0; i<n;i++) {
+				tall[i] =Integer.parseInt(st.nextToken());
+			}
+			
+			for (int i =1; i<n+1;i++) {
+				top(i,0,0,0);
+			}
+			System.out.println("#" +test+" "+result);
+			
+		}
+	}
+	static void top(int temp, int cnt,int x, int idx) {
+		
+		if(temp == cnt) {
+			if(x<b) {
+				return;
+			}
+			x= Math.abs(b-x);
+			
+			if(result>x) {
+				result=x;
+			}
+			return;
+		}
+		
+		for(int i=idx; i<n; i++) {
+			int tx =tall[i];
+			top(temp, cnt+1,x+tx,i+1);
+		}
+	}
+}
