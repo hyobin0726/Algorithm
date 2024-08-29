c7cbf0d3d295275537791151362724ea552163c5
hyobin <140376727+hyobin0726@users.noreply.github.com>
2024-08-29 11:56:14 +0900

    [D4] Title: [S/W 문제해결 기본] 10일차 - Contact, Time: 140 ms, Memory: 44,988 KB -BaekjoonHub

--- SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.py
+++ SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.py
@@ -1,27 +1,66 @@
-from collections import deque
+import java.io.*;
+import java.util.*;
+public class Solution {
+	static ArrayList<Integer>[] arr;
+	static int[] visited;
+	static int ans;
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		for (int t=1; t<11; t++) { 
+			st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int start = Integer.parseInt(st.nextToken());
+		
+			st = new StringTokenizer(br.readLine());
+			arr = new ArrayList[101];
+			visited = new int[101];
+			for (int i=0; i<=100; i++) {
+				arr[i] = new ArrayList<>();
+			}
+			for(int i=0; i<n/2; i++) {
+				
+				int from = Integer.parseInt(st.nextToken());
+				int to = Integer.parseInt(st.nextToken());
+				
+				arr[from].add(to);
+			}
+			bfs(start);
+			System.out.println("#" + t + " " +ans);
+			
+			
+			
+		}
+}
+	
+	
+	static void bfs(int x) {
+		int temp;
+		Queue<Integer> q = new LinkedList<>();
+		q.add(x);
+		visited[x] = 1;
+		ans = x;
+		
+	
+		while(!q.isEmpty()) {
+			temp = q.poll();
+			if(visited[ans] < visited[temp]|| (visited[ans] == visited[temp]&& ans <temp) ){
+				ans = temp;
+			}
+			
+			for(int i=0; i< arr[temp].size(); i++) {
+				if(visited[arr[temp].get(i)] ==0) {
+					visited[arr[temp].get(i)]  =visited[temp]+1;
+					q.add(arr[temp].get(i));
+				}
+			}
+			
+			
+		}
 
-def bfs(start):
-    visited = [0] * 101
-    q=deque()
-    q.append((start))
-    visited[start]=1
-    ans = start
-    while q :
-        x=q.popleft()
-        if visited[ans] < visited[x] or (visited[ans]==visited[x] and ans<x) :
-            ans=x
-        for i in contact[x]:
-            if not visited[i]:
-                q.append((i))
-                visited[i]=visited[x]+1
-    return ans
-for test in range(1,11):
-    t,start=map(int,input().split())
-    data=list(map(int,input().split()))
-    contact=[[]for i in range(101)]
-    for i in range (0,t,2):
-        a,b = data[i],data[i+1]
-        contact[a].append(b)
-    
-    ans=bfs(start)
-    print("#{} {}".format(test,ans))
+		
+	}
+	
+}
