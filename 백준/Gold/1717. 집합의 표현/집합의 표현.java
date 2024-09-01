3e3bf7f90eb0ac52737ea3f06b006948013275f0
hyobin <140376727+hyobin0726@users.noreply.github.com>
2024-09-02 00:28:29 +0900

    [Gold V] Title: 집합의 표현, Time: 1340 ms, Memory: 55020 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    static int n;
+    static int m;
+    static int[] parents;
+
+    static void make(){
+        parents = new int[n+1];
+        for (int i=0; i<=n; i++){
+            parents[i] = i;
+        }
+    }
+    static int find (int a){
+        if(parents[a]==a) return a;
+        return parents[a] = find(parents[a]);
+    }
+    static void union (int a, int b){
+        int aroot = find(a);
+        int broot = find(b);
+
+        if(a<b){
+            parents[broot] = aroot;
+        }
+        else{
+            parents[aroot] = broot;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        make();
+        for(int i=0; i<m; i++){
+            st = new StringTokenizer(br.readLine());
+            int op = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if(op==0){
+                union(a, b);
+            }
+            else{
+                if(find(a)==find(b)){
+                    System.out.println("YES");
+                }
+                else{
+                    System.out.println("NO");
+                }
+            }
+        }
+
+    }
+}
