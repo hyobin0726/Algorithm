53ba06e7cc8079160e470cfe8b41927815f97787
hyobin <140376727+hyobin0726@users.noreply.github.com>
2024-09-15 05:25:14 +0900

    [Bronze II] Title: Bunnies, Time: 60 ms, Memory: 11288 KB -BaekjoonHub

--- 백준/Bronze/26529. Bunnies/Bunnies.java
+++ 백준/Bronze/26529. Bunnies/Bunnies.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         dp = new int[46];
-        dp[0] = 1;
+        dp[0]=1;
         dp[1] =1;
         int n = Integer.parseInt(br.readLine());
         for(int i =0; i<n; i++){
@@ -15,10 +15,11 @@ public class Main {
         }
 
     }
-    static void fibo(int x){
-        for(int i=2; i<=x; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+    static int fibo(int x){
+        if(dp[x] ==0){
+            dp[x] = fibo(x-1) + fibo(x-2);
         }
-      
+        return dp[x];
+
     }
 }
