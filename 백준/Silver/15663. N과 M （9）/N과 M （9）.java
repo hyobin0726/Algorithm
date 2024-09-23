c8c1b17f79f3cff25d3400ee59924fe95e4f23c4
hyobin <140376727+hyobin0726@users.noreply.github.com>
2024-09-24 02:27:50 +0900

    [Silver II] Title: N과 M (9), Time: 688 ms, Memory: 93232 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static int n,m;
+    static int[] num;
+    static boolean[] visited;
+    static HashSet<String> set = new HashSet<>();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n= Integer.parseInt(st.nextToken());
+        m= Integer.parseInt(st.nextToken());
+        num = new int[n];
+        visited = new boolean[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(num);
+        dfs(0,new int[m]);
+
+    }
+    static void dfs( int cnt, int[] arr){
+        if(cnt == m){
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i<m; i++){
+                sb.append(arr[i]+" ");
+            }
+            if(!set.contains(sb.toString())){
+                set.add(sb.toString());
+                System.out.println(sb);
+            }
+            return;
+        }
+        for(int i=0; i<n; i++){
+            if(visited[i]) continue;
+            visited[i] = true;
+            arr[cnt] = num[i];
+            dfs( cnt+1, arr);
+            visited[i] = false;
+        }
+    }
+}
