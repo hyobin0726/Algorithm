f2b81b855a961cbc35d757a143733ec78b9d14d8
hyobin <140376727+hyobin0726@users.noreply.github.com>
2024-10-03 00:49:31 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 468 ms, Memory: 96268 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static int[] arr;
+    static int[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        arr=new int[n+1];
+        dp=new int[n+1];
+        st= new StringTokenizer(br.readLine());
+        for(int i=1; i<=n;i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        dp[1] = arr[1];
+        int idx = 2;
+        for(int i=2; i<=n; i++){
+            int temp = arr[i];
+
+            if(dp[idx-1] < temp){
+                dp[idx] = temp;
+                idx++;
+            }else {
+                int s = 1;
+                int e = idx;
+
+                while (s<e){
+                    int mid = (s+e)/2;
+
+                    if(dp[mid] < temp){
+                        s = mid+1;
+                    }else {
+                        e = mid;
+                    }
+                }
+                dp[s] = temp;
+            }
+        }
+        System.out.println(idx-1);
+
+    }
+}
