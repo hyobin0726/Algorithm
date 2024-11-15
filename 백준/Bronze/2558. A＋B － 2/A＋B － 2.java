c886ad7f26a8cab5559466ac76d4aca1378c9079
hyobin <140376727+hyobin0726@users.noreply.github.com>
2024-11-15 20:12:32 +0900

    [Bronze V] Title: A+B - 2, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int sum = a+b;
+        StringBuilder sb = new StringBuilder();
+        sb.append(sum);
+        System.out.println(sb);
+    }
+
+}
