310d92df13c1d93ddab37af11cdafeda895eae3a
hyobin <140376727+hyobin0726@users.noreply.github.com>
2024-12-10 21:39:54 +0900

    [Gold V] Title: 다각형의 면적, Time: 180 ms, Memory: 18784 KB -BaekjoonHub

--- 백준/Gold/2166. 다각형의 면적/다각형의 면적.java
+++ 백준/Gold/2166. 다각형의 면적/다각형의 면적.java
@@ -1,40 +1,37 @@
 import java.io.*;
 import java.util.*;
-
 public class Main {
     static int n;
-    static List<double[]> num; 
+    static List<int[]> num;
     static double ans;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         num = new ArrayList<>();
         StringTokenizer st;
-
-        for (int i = 0; i < n; i++) {
+        for(int i=0; i<n; i++){
             st = new StringTokenizer(br.readLine());
-            double x = Double.parseDouble(st.nextToken());
-            double y = Double.parseDouble(st.nextToken());
-            num.add(new double[] {x, y});
+            int x= Integer.parseInt(st.nextToken());
+            int y= Integer.parseInt(st.nextToken());
+            num.add(new int[] {x,y});
         }
+        double x= num.get(0)[0];
+        double y = num.get(0)[1];
         ans = 0;
-        for (int i = 1; i < n - 1; i++) {
-            double x = num.get(0)[0];
-            double y = num.get(0)[1];
-            double x1 = num.get(i)[0];
-            double y1 = num.get(i)[1];
-            double x2 = num.get(i + 1)[0];
-            double y2 = num.get(i + 1)[1];
 
-            double temp1 = x * y1 + x1 * y2 + x2 * y;
-            double temp2 = x1 * y + x2 * y1 + x * y2;
+        for(int i=1; i<n-1; i++){
+            double x1 =  num.get(i)[0];
+            double y1 =  num.get(i)[1];
+            double x2 =  num.get(i+1)[0];
+            double y2 =  num.get(i+1)[1];
 
-            ans += (temp1 - temp2); 
+            double temp1 = (double) x*y1 + x1*y2 + x2*y;
+            double temp2 =  (double) x1*y + x2*y1 + x*y2;
+
+            ans += temp1-temp2;
         }
-
-        ans = Math.abs(ans) / 2.0; 
-
+        ans = Math.abs(ans) / 2.0;
         System.out.printf("%.1f\n", ans);
+
     }
 }
