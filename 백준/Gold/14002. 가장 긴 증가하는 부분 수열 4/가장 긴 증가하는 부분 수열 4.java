d63dc6f79966b7812a8fead1e8a530beca07fa51
hyobin <140376727+hyobin0726@users.noreply.github.com>
2025-01-03 12:17:45 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 140 ms, Memory: 16224 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -1,49 +1,54 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
-    static int[] arr;
-    static int[] dp;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int n = Integer.parseInt(br.readLine());
-        arr=new int[n+1];
-        dp=new int[n+1];
-        ArrayList<Integer>[] dplist = new ArrayList[n+1];
-        for(int i=1; i<= n; i++){
-            dplist[i] = new ArrayList<>();
-        }
-
-        st= new StringTokenizer(br.readLine());
-        for(int i=1; i<=n;i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        dp[1] = 1;
-        dplist[1].add(arr[1]);
-        int max = dp[1];
-        int idx = 1;
-        for(int i=2; i<=n ; i++){
-            dp[i] = 1;
-            dplist[i].add(arr[i]);
-            for(int j=1; j<i; j++){
-                if(arr[i] > arr[j] && dp[i]<= dp[j]){
-                    dp[i] = dp[j]+1;
-                    dplist[i]= new ArrayList<>(dplist[j]);
-                    dplist[i].add(arr[i]);
-                }
-            }
-            if(max<dp[i]){
-                max = dp[i];
-                idx = i;
-            }
-        }
-        System.out.println(max);
-//        System.out.println(idx);
-        StringBuilder sb = new StringBuilder();
-        for(int t : dplist[idx]){
-            sb.append(t + " ");
-        }
-        System.out.println(sb);
-
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Stack<Integer> stack = new Stack<>();
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		int[] dp = new int[n];
+		int length = 0;
+		int max_idx = 0;
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			// arr 현재 요소 받기
+			int ele = Integer.parseInt(st.nextToken());
+			arr[i] = ele;
+			
+			// 현재 요소를 받고 dp 시작
+			dp[i] = 1;
+			for(int j = 0; j < i; j++) {
+				if(arr[j] < ele) {
+					dp[i] = Math.max(dp[j] + 1, dp[i]);
+				}
+			}
+			
+			// 최댓값, 최대인덱스를 저장한다.
+			if(length < dp[i]) {
+				length = dp[i];
+				max_idx = i;
+			}
+		}
+		
+		System.out.println(length);
+		
+		for(int i = max_idx; i >= 0; i--) {
+			if(length == dp[i]) {
+				stack.add(arr[i]);
+				length--;
+			}
+		}
+		
+		while(!stack.isEmpty()) {
+			sb.append(stack.pop() + " ");
+		}
+		
+		System.out.println(sb);
+		
+	}
 }
