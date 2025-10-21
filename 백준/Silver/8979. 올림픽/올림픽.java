2b69cc76db861b40a174842b4e7cd9b306495bf9
hyobin <140376727+hyobin0726@users.noreply.github.com>
2025-10-21 23:18:46 +0900

    [Silver V] Title: 올림픽, Time: 124 ms, Memory: 14612 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -3,53 +3,34 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int n;
-        int k;
-        int[][] medal;
-        boolean[] check;
-        int ans=1;
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-        medal = new int[n+1][3];
-        check = new boolean[n+1];
-        for (int i =0; i<n ; i++){
+        int[][] medal = new int[n + 1][3]; // [국가][0:금,1:은,2:동]
+
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            int tmp = Integer.parseInt(st.nextToken());
-            for(int j=0; j<3; j++){
-                medal[tmp][j] = Integer.parseInt(st.nextToken());
-            }
-            check[tmp] = false;
+            int id = Integer.parseInt(st.nextToken());
+            medal[id][0] = Integer.parseInt(st.nextToken()); // 금
+            medal[id][1] = Integer.parseInt(st.nextToken()); // 은
+            medal[id][2] = Integer.parseInt(st.nextToken()); // 동
         }
 
-        for (int i =0; i<3 ; i++){
-            boolean is= false;
-            for(int j=1; j<n+1; j++){
-                if(medal[k][i] == 0 && !check[j]){
-                    if(medal[k][i] <medal[j][i]){
-                        check[j] = true;
-                        ans++;
-                    }
-                }else {
-                    is = true;
-                    if(medal[k][i] <medal[j][i] && !check[j]){
-                        check[j] = true;
-                        ans++;
-                    }
-                }
-            }
-            if(is){
-                break;
+        int ans = 1; // 기본 순위 1위
+        for (int j = 1; j <= n; j++) {
+            if (j == k) continue;
+            // j가 k보다 사전식으로 “엄격히 더 크면” (금> || (금== && 은>) || (금== && 은== && 동>))
+            if (medal[j][0] > medal[k][0]
+                || (medal[j][0] == medal[k][0] && medal[j][1] > medal[k][1])
+                || (medal[j][0] == medal[k][0] && medal[j][1] == medal[k][1] && medal[j][2] > medal[k][2])) {
+                ans++;
             }
         }
+
         System.out.println(ans);
-
-
     }
-
 }
